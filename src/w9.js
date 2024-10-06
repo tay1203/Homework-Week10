13faf766d832cc755194314548e26f37ae2a9289
Tay Ming Hui <mtay0031@student.monash.edu>
2024-10-06 16:13:39 +0800

    edit path

--- src/w9.js
+++ src/w9.js
@@ -1,9 +1,9 @@
-var vg_1 = "/src/w9.json";
+var vg_1 = "./src/w9.json";
 vegaEmbed("#map", vg_1).then(function(result){
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-var vg_2 = "/src/w10.json";
+var vg_2 = "./src/w10.json";
 vegaEmbed("#bar", vg_2).then(function(result){
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
